dec2a283d84d6122e9a2b6261e55a4609dab250c
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 14:46:04 -0500

    re test

---
+++ bin/ft_test/ft_balance_of_test.ts
@@ -0,0 +1,7 @@
+import { near_kit_env, ft_balance_of_fun } from "../../src/index";
+// ==============================================
+const shit_ca = "shit-237.factory.v10.meme-cooking.testnet";
+const account_id = "sleet.testnet";
+// ==============================================
+console.log(await ft_balance_of_fun(near_kit_env, shit_ca, account_id));
+// ==============================================
