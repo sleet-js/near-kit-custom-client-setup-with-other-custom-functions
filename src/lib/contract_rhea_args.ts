a35ec42837b7e9e0440c5e959b0d3e67bf338ae3
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 08:31:55 -0500

    re

---
+++ src/lib/contract_rhea_args.ts
@@ -0,0 +1,18 @@
+// ================================================
+export interface ref_args_params_interface {
+  pool_id: number;
+  from_index: number;
+  limit: number;
+  fee: number;
+  tokens: string[];
+  account_id: string;
+  token_id: string;
+  amount: string;
+  deposit: string;
+  token_in: string;
+  amount_in: string;
+  token_out: string;
+  min_amount_out: string;
+  referral_id: string;
+}
+// ================================================
