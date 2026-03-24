f13c17c8bd4cbf753a0c6cda971d5bd903fb7f73
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-23 22:13:07 -0500

    Update contract_greeting_const.ts

--- src/lib/contract_greeting_const.ts
+++ src/lib/contract_greeting_const.ts
@@ -1,7 +1,29 @@
+import { MY_BACKEND_NEAR_networkId } from "../new-env";
+import { getStoredNetworkId } from "../new-hot";
+// ===========================================
+const hello_testnet_contractId_const = "hello.sleet.testnet";
+const hello_mainnet_contractId_const = "hello.sleet.near";
+// ===========================================
+// env
+export const hello_contractId_for_network_env =
+  MY_BACKEND_NEAR_networkId === "testnet"
+    ? "hello.sleet.testnet"
+    : "hello.sleet.near";
+// ===========================================
+// web
+export function hello_contractId_for_network_web() {
+  const networkId = getStoredNetworkId();
+  const hello_contractId =
+    networkId === "testnet"
+      ? hello_testnet_contractId_const
+      : hello_mainnet_contractId_const;
+  console.log("hello contractId", hello_contractId);
+  return hello_contractId;
+}
 // ===========================================
 // contact methods
 export const greeting_contract_methods = {
   get_greeting: "get_greeting",
   set_greeting: "set_greeting",
-}
+};
 // ===========================================
