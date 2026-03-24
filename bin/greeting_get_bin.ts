b58b2cbb35102cffa42818a8563887afc7c906bb
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-23 22:18:31 -0500

    Update greeting_get_bin.ts

---
+++ bin/greeting_get_bin.ts
@@ -0,0 +1,10 @@
+import { hello_contractId_for_network_env } from "../src/lib/contract_greeting_const";
+import { greeting_get_greeting_fun } from "../src/fun/greeting/greeting_get_fun";
+import { near_kit_env } from "../src/new-env";
+// ==============================================
+console.log(
+  await greeting_get_greeting_fun(
+    near_kit_env,
+    hello_contractId_for_network_env,
+  ),
+);
