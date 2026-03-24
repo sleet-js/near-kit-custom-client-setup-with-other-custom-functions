909f23589d48c509a14143ef217dab0da466368e
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 13:37:08 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,29 @@ export {
   MY_ENV_NEAR_networkId,
   MY_ENV_NEAR_accountId,
   MY_ENV_NEAR_publicKey,
-  MY_ENV_NEAR_privateKey
+  MY_ENV_NEAR_privateKey,
 } from "./new-env";
+// ===========================================
+// utility
+// ==== greeting ====
+export type { greeting_args_params_interface } from "./lib/contract_greeting_args";
+export {
+  hello_contractId_for_network_env,
+  hello_contractId_for_network_web,
+  greeting_contract_methods,
+} from "./lib/contract_greeting_const";
+// ==== rhea ====
+export type { ref_args_params_interface } from "./lib/contract_rhea_args";
+export {
+  rhea_contractId_for_network_env,
+  rhea_contractId_for_network_web,
+  ref_exchange_methods_const,
+} from "./lib/contract_rhea_const";
+// ==== wrap near ====
+export {
+  wrap_contractId_for_network_env,
+  wrap_contractId_for_network_web,
+  wrap_near_methods_const
+} from "./lib/contract_near_wrap_const";
 // ===========================================
 // copyright 2026 by sleet.near
