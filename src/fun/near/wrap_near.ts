f8bdee30dfb37f3853fccc26453dcbd4700691a2
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 14:03:31 -0500

    Update wrap_near.ts

--- src/fun/near/wrap_near.ts
+++ src/fun/near/wrap_near.ts
@@ -1,5 +1,5 @@
 import { Near } from "near-kit";
-import type { FinalExecutionOutcome, } from "near-kit";
+import type { FinalExecutionOutcome } from "near-kit";
 import { wrap_near_methods_const } from "@sleet-js/wrap-near-contract-methods-const";
 import type { AmountInput } from "../../types/AmountInput";
 // ===========================================
@@ -14,6 +14,20 @@ export async function wrap_near_deposit_fun(
     wrap_near_methods_const.near_deposit,
     {}, // no args
     { attachedDeposit: amount }, // how much NEAR to wrap
+  );
+  return result as FinalExecutionOutcome;
+}
+// ===========================================
+export async function wrap_near_withdraw_fun(
+  near: Near,
+  wrap_contractId: string,
+  amount: string, // amount to withdraw in decimal example "100000000000000000000000"
+) {
+  const result = await near.call(
+    wrap_contractId,
+    wrap_near_methods_const.near_withdraw,
+    { amount: amount },
+    { attachedDeposit: "1 yocto" },
   );
   return result as FinalExecutionOutcome;
 }
