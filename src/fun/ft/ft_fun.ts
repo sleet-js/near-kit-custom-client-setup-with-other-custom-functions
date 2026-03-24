a204ebda70621b76c1e5601a3be7c3b330da4cb3
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 15:59:40 -0500

    re

--- src/fun/ft/ft_fun.ts
+++ src/fun/ft/ft_fun.ts
@@ -1,11 +1,14 @@
 import { Near } from "near-kit";
 import { ft_methods_const } from "@sleet-js/ft-methods-const";
 import type { ft_args_params_interface } from "../../lib/contract_ft_args";
+import type { FinalExecutionOutcome } from "near-kit";
 // ===========================================
 // view
 // ft_balance_of
 // call
+// ft_transfer
 // ===========================================
+// ft_balance_of
 export async function ft_balance_of_fun(
   near: Near,
   ft_contractId: string,
@@ -17,5 +20,19 @@ export async function ft_balance_of_fun(
     { account_id: account_id },
   );
   return result as string;
+}
+// ===========================================
+// ft_transfer
+export async function ft_transfer_fun(
+  near: Near,
+  ft_contractId: string,
+  receiver_id: ft_args_params_interface["receiver_id"],
+) {
+  const result = await near.call(
+    ft_contractId,
+    ft_methods_const.ft_transfer,
+    { receiver_id: receiver_id },
+  );
+  return result as FinalExecutionOutcome;
 }
 // ===========================================
