6e7b021f9593ebcfe906ba54551b87363f3dcb86
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 08:42:08 -0500

    re

--- bin/greeting_set_bin.ts
+++ bin/greeting_set_bin.ts
@@ -2,10 +2,13 @@ import { hello_contractId_for_network_env } from "../src/lib/contract_greeting_c
 import { greeting_set_greeting_fun } from "../src/fun/greeting/greeting_set_fun";
 import { near_kit_env } from "../src/new-env";
 // ==============================================
+const greeting = "Hello from near kit bin";
+// ==============================================
 console.log(
   await greeting_set_greeting_fun(
     near_kit_env,
     hello_contractId_for_network_env,
+    greeting,
   ),
 );
 // ==============================================
--- src/lib/contract_greeting_args.ts
+++ src/lib/contract_greeting_args.ts
@@ -1,5 +1,5 @@
 // ================================================
 export interface greeting_args_params_interface {
-  set_greeting: string;
+  greeting: string;
 }
 // ================================================
