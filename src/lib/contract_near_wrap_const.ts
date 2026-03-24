543e602829380c54debfbb1bbf95596893669020
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 10:28:53 -0500

    re

--- src/lib/contract_near_wrap_const.ts
+++ src/lib/contract_near_wrap_const.ts
@@ -1,7 +1,24 @@
+export { wrap_near_methods_const } from '@sleet-js/wrap-near-contract-methods-const';
 import { MY_BACKEND_NEAR_networkId } from "../new-env";
 import { getStoredNetworkId } from "../new-hot";
 // ===========================================
 const wrap_testnet_contractId_const = "wrap.testnet";
 const wrap_mainnet_contractId_const = "wrap.near";
 // ===========================================
+// env
+export const wrap_contractId_for_network_env =
+  MY_BACKEND_NEAR_networkId === "testnet"
+    ? wrap_testnet_contractId_const
+    : wrap_mainnet_contractId_const;
+// ===========================================
+// web
+export function wrap_contractId_for_network_web() {
+  const networkId = getStoredNetworkId();
+  const wrap_contractId =
+    networkId === "testnet"
+      ? wrap_testnet_contractId_const
+      : wrap_mainnet_contractId_const;
+  console.log("wrap near contractId", wrap_contractId);
+  return wrap_contractId;
+}
 // ===========================================
