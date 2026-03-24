6597d0389aa6da117eceeb6f095437c64de93c62
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-24 10:25:01 -0500

    Create contract_near_wrap_const.ts

---
+++ src/lib/contract_near_wrap_const.ts
@@ -0,0 +1,7 @@
+import { MY_BACKEND_NEAR_networkId } from "../new-env";
+import { getStoredNetworkId } from "../new-hot";
+// ===========================================
+const wrap_testnet_contractId_const = "wrap.testnet";
+const wrap_mainnet_contractId_const = "wrap.near";
+// ===========================================
+// ===========================================
