423c522ab0c43d1604724348479245966571492d
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-25 15:59:51 -0500

    Update rhea_call_test.ts

--- bin/rhea_test/rhea_call_test.ts
+++ bin/rhea_test/rhea_call_test.ts
@@ -13,14 +13,36 @@ import {
   ref_withdraw_function,
   ref_add_simple_pool_function,
 } from "../../src/index";
+import type { ref_swap_action_type } from "../../src/index";
 // ==============================================
 // complete flow
+// 1. wrap NEAR
+// 2. rhea storage deposit
+// 2. deposit/transfer to rhea
+// 3. swap
+// 4. withdraw ❌
+// 5. un wrap ❌
 // ==============================================
 const amount_near: AmountInput = "2 NEAR";
 const ft_contractId = wrap_contractId_for_network_env;
 const receiver_id = rhea_contractId_for_network_env;
 const msg = "";
 const amount = "2000000000000000000000000"; // 2 NEAR
+const swap_actions: ref_swap_action_type[] = [
+  {
+    pool_id: 2459,
+    token_in: "wrap.testnet",
+    amount_in: "2000000000000000000000000",
+    token_out: "shit-237.factory.v10.meme-cooking.testnet",
+    min_amount_out: "0",
+  },
+  {
+    pool_id: 2459,
+    token_in: "shit-237.factory.v10.meme-cooking.testnet",
+    token_out: "wrap.testnet",
+    min_amount_out: "0",
+  },
+];
 // ==============================================
 console.log("==============================================");
 console.log(
@@ -36,6 +58,17 @@ console.log(
     receiver_id,
     msg,
     amount,
+  ),
+);
+console.log("==============================================");
+// ==============================================
+console.log("==============================================");
+console.log(
+  await ref_swap_function(
+    near_kit_env,
+    rhea_contractId_for_network_env,
+    swap_actions,
+    "ref.huggies.testnet",
   ),
 );
 console.log("==============================================");
