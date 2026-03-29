bdf5a57bfd5ab5cfc83bdd303681be67f19be8b1
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-03-29 09:03:20 -0500

    test?
      "version": "0.0.11",

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // ===========================================
-// ===========================================
 // web
 // near_kit_client
 export {
@@ -17,53 +16,23 @@ export {
 // archival
 export { near_kit_client_archival } from "./new-archival";
 // ===========================================
-// ===========================================
-// backend
-// near_kit_env
+// utility - contract constants (web versions)
 export {
-  near_kit_env,
-  MY_ENV_NEAR_networkId,
-  MY_ENV_NEAR_accountId,
-  MY_ENV_NEAR_publicKey,
-  MY_ENV_NEAR_privateKey,
-} from "./new-env";
-// ===========================================
-// ===========================================
-// utility
-// ==== greeting ====
-export type { greeting_args_params_interface } from "./lib/contract_greeting_args";
-export {
-  hello_contractId_for_network_env,
   hello_contractId_for_network_web,
   greeting_contract_methods,
-} from "./lib/contract_greeting_const";
-// ==== rhea ====
-export type {
-  ref_args_params_interface,
-  ref_swap_action_type,
-  ref_swap_function_args,
-} from "./lib/contract_rhea_args";
+} from "./lib/contract_greeting_const_web";
 export {
-  rhea_contractId_for_network_env,
   rhea_contractId_for_network_web,
   ref_exchange_methods_const,
-} from "./lib/contract_rhea_const";
-// ==== ft ====
-export { ft_methods_const } from "@sleet-js/ft-methods-const";
-export type { ft_args_params_interface } from "./lib/contract_ft_args";
-// ==== wrap near ====
+} from "./lib/contract_rhea_const_web";
 export {
-  wrap_contractId_for_network_env,
   wrap_contractId_for_network_web,
   wrap_near_methods_const,
-} from "./lib/contract_near_wrap_const";
+} from "./lib/contract_near_wrap_const_web";
 // ===========================================
-// ===========================================
-// fun
-// ==== greeting ====
+// fun (web-compatible functions)
 export { greeting_get_greeting_fun } from "./fun/greeting/greeting_get_fun";
 export { greeting_set_greeting_fun } from "./fun/greeting/greeting_set_fun";
-// ==== rhea ====
 export {
   ref_get_number_of_pools_function,
   ref_get_deposits_function,
@@ -78,22 +47,25 @@ export {
   ref_withdraw_function,
   ref_add_simple_pool_function,
 } from "./fun/rhea/rhea_call_fun";
-// ==== ft ====
 export {
   ft_balance_of_fun,
   ft_transfer_fun,
   ft_transfer_call_fun,
   ft_storage_deposit_fun,
 } from "./fun/ft/ft_fun";
-// ==== wrap near ====
 export {
   wrap_near_deposit_fun,
   wrap_near_withdraw_fun,
 } from "./fun/near/wrap_near";
 // ===========================================
-// ===========================================
 // types
 export type { AmountInput } from "./types/AmountInput";
-// ===========================================
+export type { greeting_args_params_interface } from "./lib/contract_greeting_args";
+export type {
+  ref_args_params_interface,
+  ref_swap_action_type,
+  ref_swap_function_args,
+} from "./lib/contract_rhea_args";
+export type { ft_args_params_interface } from "./lib/contract_ft_args";
 // ===========================================
 // copyright 2026 by sleet.near
